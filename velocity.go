c003107d16d698a7c4bf8ac9fd9b3d6c4ffd3449
Leo Neumeyer <leoneu@apache.org>
2013-05-12 19:03:18 -0300

    Change distance to meter to use SI base unit consistently.

--- velocity.go
+++ velocity.go
@@ -8,36 +8,30 @@ import (
 type Velocity float64
 
 const (
-	NanometerPerSecond           = 1e-6 * MillimeterPerSecond
-	MicrometerPerSecond          = 1e-3 * MillimeterPerSecond
-	MillimeterPerSecond Velocity = 1
-	CentimeterPerSecond          = 1e1 * MillimeterPerSecond
-	MeterPerSecond               = 1e3 * MillimeterPerSecond
+	NanometerPerSecond           = 1e-9 * MeterPerSecond
+	MicrometerPerSecond          = 1e-6 * MeterPerSecond
+	MillimeterPerSecond          = 1e-3 * MeterPerSecond
+	CentimeterPerSecond          = 1e-2 * MeterPerSecond
+	MeterPerSecond      Velocity = 1
 	KilometerPerHour             = 1e3 * MeterPerSecond / 60 / 60
 )
 
 func (v Velocity) MillimetersPerSecond() float64 {
-	return float64(v/MillimeterPerSecond)
+	return float64(v / MillimeterPerSecond)
 }
 
 func (v Velocity) CentimetersPerSecond() float64 {
-	return float64(v/CentimeterPerSecond)
+	return float64(v / CentimeterPerSecond)
 }
 
 func (v Velocity) MetersPerSecond() float64 {
-	return float64(v/MeterPerSecond)
+	return float64(v / MeterPerSecond)
 }
 
-func (v Velocity)KilometersPerSecond() float64 {
-	return float64(v/KilometerPerHour)
+func (v Velocity) KilometersPerSecond() float64 {
+	return float64(v / KilometerPerHour)
 }
 
 func (v Velocity) MultiplyWithDuration(t time.Duration) Distance {
-	return Distance(float64(v)*float64(t)/float64(time.Second))
+	return Distance(float64(v) * float64(t) / float64(time.Second))
 }
-
-// Backwards compatibility only
-//func TimesVelocityDuration(v Velocity, t time.Duration) Distance {
-//	return v.MultiplyWithDuration(t)
-//}
-
